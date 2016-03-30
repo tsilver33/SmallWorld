848434837061f18cbc5288fc57f28b9bfe363bb1
Tyler <Tyler@10.200.2.244>
2016-03-30 10:51:14 -0500

    Version 1.0 No Changes to the initial project.

---
+++ src/smallworld/GraphGenerator.java
@@ -0,0 +1,36 @@
+package smallworld;
+
+import edu.princeton.cs.In;
+import edu.princeton.cs.StdOut;
+
+/******************************************************************************
+ *  Compilation:  javac GraphGenerator.java
+ *  Execution:    java GraphGenerator filename delimiter
+ *  Dependencies: Graph.java In.java StdOut.java
+ *
+ ******************************************************************************/
+
+public class GraphGenerator {
+
+    public static Graph read(In in, String delimiter) {
+        Graph G = new Graph();
+        while (in.hasNextLine()) {
+            String line = in.readLine();
+            String[] names = line.split(delimiter);
+            String movie = names[0];
+            for (int i = 1; i < names.length; i++) {
+                G.addEdge(movie, names[i]);
+            }
+        }
+        return G;
+    }
+
+    public static void main(String[] args) {
+        String filename  = args[0];
+        String delimiter = args[1];
+        In in = new In(filename);
+        Graph G = GraphGenerator.read(in, delimiter);
+        StdOut.println(G);
+    }
+
+}
